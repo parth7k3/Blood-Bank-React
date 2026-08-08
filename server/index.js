4692e6597213a788125c9e9d621ece476fbdcbee
parth7k3 <parthjoshisrs@gmail.com>
2026-08-08 19:23:27 +0530

    fix(build): bundle .env securely into ASAR and resolve path explicitly for SMTP configuration

--- server/index.js
+++ server/index.js
@@ -6,8 +6,9 @@ const path = require('path');
 const os = require('os');
 const db = require('./db');
 const nodemailer = require('nodemailer');
+const path = require('path');
 
-require('dotenv').config();
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
